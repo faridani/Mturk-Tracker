a3e976ad1cf2914bd14b7dd0b929a5d9ddd72d2b
mickek <mklujszo@gmail.com>
2009-10-23 22:03:47 +0200

    missing lib

--- libs/tenclouds/sql.py
+++ libs/tenclouds/sql.py
@@ -18,6 +18,21 @@ def query_to_dicts(query_string, *query_args):
         yield row_dict
     return
 
+def query_to_tuples(query_string, *query_args):
+    """Run a simple query and produce a generator
+    that returns the results as a bunch of dictionaries
+    with keys for the column values selected.
+    """
+    cursor = connection.cursor() #@UndefinedVariable
+    cursor.execute(query_string, query_args)
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        yield row
+    return
+
+
 def execute_sql(query_string, *query_args):
     
     cursor = connection.cursor() #@UndefinedVariable
