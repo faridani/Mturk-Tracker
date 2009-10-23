2de440fb97def438bf261b50e562db290bab220d
mickek <mklujszo@gmail.com>
2009-10-23 12:09:10 +0200

    fixed graphs

---
+++ src/mturk/main/management/commands/__init__.py
@@ -0,0 +1,22 @@
+from tenclouds.sql import query_to_dicts, execute_sql
+import logging
+def clean_duplicates():
+    
+    ids = query_to_dicts("select group_id from main_hitgroupcontent group by group_id having count(*) > 1;")
+    
+    for id in ids:
+        print "deleting %s" % id['group_id']
+        logging.info( "deleting %s" % id['group_id'] )
+        
+        execute_sql("""delete from main_hitgroupstatus where 
+                        hit_group_content_id  in (
+                            select id from main_hitgroupcontent where id != 
+                                (select min(id) from main_hitgroupcontent where group_id = '%s') 
+                        and group_id = '%s');            
+        """ % (id['group_id'], id['group_id']))
+        
+        execute_sql("""delete from main_hitgroupcontent where 
+                        id != (select min(id) from main_hitgroupcontent where group_id = '%s') and group_id = '%s'
+                    """ % (id['group_id'], id['group_id']))
+        
+    execute_sql('commit;')            
--- src/mturk/main/management/commands/db_refresh_mviews.py
+++ src/mturk/main/management/commands/db_refresh_mviews.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from tenclouds.sql import execute_sql
+from mturk.main.management.commands import clean_duplicates
 import time
 import logging
 
@@ -9,12 +10,17 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         
+        logging.info('cleaning up db from duplicates')
+        
+        clean_duplicates()  
+        
         logging.info('Refreshing materialised views')
         
         start_time = time.time()
         execute_sql("select incremental_refresh_matview('hits_mv');")
         execute_sql("analyze hits_mv;")
         execute_sql("commit;")
+        
         log = 'refreshing hits_mv took: %s' % (time.time() - start_time)
         logging.info(log)
         print log
