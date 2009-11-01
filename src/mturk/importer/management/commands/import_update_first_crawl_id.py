82bd06076f2145918e15d791485c82b2675f898b
mickek <mklujszo@gmail.com>
2009-11-01 15:31:23 +0100

    erorr reporting and rollback

--- src/mturk/importer/management/commands/import_update_first_crawl_id.py
+++ src/mturk/importer/management/commands/import_update_first_crawl_id.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from tenclouds.sql import query_to_dicts, execute_sql
 from mturk.main.models import Crawl
+from mturk.main.management.commands.crawler_common import grab_error
+import sys
 import datetime
 
 def get_first_crawl():
@@ -16,18 +18,25 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         
-        commit_threshold = 1000
-        results = query_to_dicts("select id from main_hitgroupcontent where first_crawl_id is null")
-        
-        for i, row in enumerate(results):
+        try:
             
-            execute_sql("""update main_hitgroupcontent p set first_crawl_id = 
-                (select min(crawl_id) from main_hitgroupstatus where group_id = p.group_id)
-                where 
-                    id = %s
-            """ % row['id'])                    
+            commit_threshold = 1000
+            results = query_to_dicts("select id from main_hitgroupcontent where first_crawl_id is null")
             
-            if i % commit_threshold == 0:
-                print datetime.datetime.now(), 'commited after %s rows' % i
-                execute_sql('commit;')  
+            for i, row in enumerate(results):
+                
+                execute_sql("""update main_hitgroupcontent p set first_crawl_id = 
+                    (select min(crawl_id) from main_hitgroupstatus where group_id = p.group_id)
+                    where 
+                        id = %s
+                """ % row['id'])                    
+                
+                if i % commit_threshold == 0:
+                    print datetime.datetime.now(), 'commited after %s rows' % i
+                    execute_sql('commit;')
+                
+        except:
+            error_info = grab_error(sys.exc_info())
+            print 'an error occured at: %s line, %s %s' % (i,error_info['type'], error_info['value'])
+            execute_sql('rollback;')                  
             
--- src/mturk/importer/management/commands/import_update_mviews.py
+++ src/mturk/importer/management/commands/import_update_mviews.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from tenclouds.sql import query_to_dicts, execute_sql
 from mturk.main.models import Crawl
+from mturk.main.management.commands.crawler_common import grab_error
+import sys
 import datetime
 
 def get_first_crawl():
@@ -16,21 +18,28 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         
-        commit_threshold = 1
-        results = query_to_dicts("select id from main_crawl p where not exists(select id from hits_mv where crawl_id = p.id)")
+        try:
         
-        for i, row in enumerate(results):
+            commit_threshold = 1
+            results = query_to_dicts("select id from main_crawl p where not exists(select id from hits_mv where crawl_id = p.id)")
             
-            execute_sql("""insert into hits_mv 
-SELECT p.id AS status_id, q.id AS content_id, p.group_id, p.crawl_id, ( SELECT main_crawl.start_time
-           FROM main_crawl
-          WHERE main_crawl.id = p.crawl_id) AS start_time, q.requester_id, p.hits_available, p.page_number, p.inpage_position, p.hit_expiration_date, q.reward, q.time_alloted
-   FROM main_hitgroupstatus p
-   JOIN main_hitgroupcontent q ON q.group_id::text = p.group_id::text AND p.hit_group_content_id = q.id
-  WHERE p.crawl_id = %s            
-            """ % row['id'])                    
+            for i, row in enumerate(results):
+                
+                execute_sql("""insert into hits_mv 
+    SELECT p.id AS status_id, q.id AS content_id, p.group_id, p.crawl_id, ( SELECT main_crawl.start_time
+               FROM main_crawl
+              WHERE main_crawl.id = p.crawl_id) AS start_time, q.requester_id, p.hits_available, p.page_number, p.inpage_position, p.hit_expiration_date, q.reward, q.time_alloted
+       FROM main_hitgroupstatus p
+       JOIN main_hitgroupcontent q ON q.group_id::text = p.group_id::text AND p.hit_group_content_id = q.id
+      WHERE p.crawl_id = %s            
+                """ % row['id'])                    
+                
+                if i % commit_threshold == 0:
+                    print datetime.datetime.now(), 'commited after %s crawls' % i
+                    execute_sql('commit;')  
             
-            if i % commit_threshold == 0:
-                print datetime.datetime.now(), 'commited after %s crawls' % i
-                execute_sql('commit;')  
             
+        except:
+            error_info = grab_error(sys.exc_info())
+            print 'an error occured at: %s line, %s %s' % (i,error_info['type'], error_info['value'])
+            execute_sql('rollback;')  
