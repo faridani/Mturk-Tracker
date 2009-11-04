89d1d3042625eea5275731477f5ff8bb3d611933
mickek <mklujszo@gmail.com>
2009-11-04 13:31:48 +0100

    remove print's

--- src/mturk/tabs_context_processor.py
+++ src/mturk/tabs_context_processor.py
@@ -4,8 +4,7 @@ def tabs(request):
     path = request.path_info
 
     top_tab = 'start'
-
-    print path    
+    
     if path.startswith('/requester_details') or path.startswith('/hit/'):
         top_tab = 'top_requesters'
     elif path != "/":
