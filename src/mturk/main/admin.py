88dcabacf5ae7d6a2d8e07bd2e6b442b1a6ab433
Piotr Husiatyński <phusiatynski@gmail.com>
2010-11-23 15:14:39 +0100

    code cleanup

--- src/mturk/main/admin.py
+++ src/mturk/main/admin.py
@@ -13,12 +13,13 @@ from models import RequesterProfile, HitGroupContent
 
 @login_required
 def top_requesters(request):
-    def row_formatter(input):
 
+    def row_formatter(input):
+        "Yield formatted rows"
         for cc in input:
             row = []
-            row.append('<a href="%s">%s</a>' % (reverse('requester_details',kwargs={'requester_id':cc[0]}) ,cc[1]))
-
+            url = reverse('requester_details', kwargs={'requester_id':cc[0]})
+            row.append('<a href="%s">%s</a>' % (url, cc[1]))
             row.append('<a href="https://www.mturk.com/mturk/searchbar?requesterId=%s" target="_mturk">%s</a> (<a href="http://feed.crowdsauced.com/r/req/%s">RSS</a>)'
                        % (cc[0],cc[0],cc[0]) )
             row.extend(cc[2:6])
@@ -26,25 +27,28 @@ def top_requesters(request):
             row.append('<a href="%s">%s</a>' % (url, cc[6] and 'public' or 'private'))
             yield row
 
+    date_30_days_before = datetime.date.today() - datetime.timedelta(days=30)
     data = row_formatter(query_to_tuples('''
-                                            select
-                                                h.requester_id,
-                                                h.requester_name,
-                                                count(*) as "projects",
-                                                sum(h.hits_available) as "hits",
-                                                sum(h.hits_available*reward) as "reward",
-                                                max(h.occurrence_date) as "last_posted",
-                                                coalesce(p.is_public, true) as is_public
-                                            from
-                                                main_hitgroupfirstoccurences h
-                                                    LEFT JOIN main_requesterprofile p ON h.requester_id = p.requester_id
-                                            where
-                                                h.occurrence_date > TIMESTAMP '%s'
-                                            group by h.requester_id, h.requester_name, p.is_public
-                                            order by sum(h.hits_available*reward) desc
-                                            limit 1000;
-''' % ( (datetime.date.today() - datetime.timedelta(days=30)).isoformat() )
-))
+            SELECT
+                h.requester_id,
+                h.requester_name,
+                count(*) as "projects",
+                sum(h.hits_available) as "hits",
+                sum(h.hits_available*reward) as "reward",
+                max(h.occurrence_date) as "last_posted",
+                coalesce(p.is_public, true) as is_public
+            FROM
+                main_hitgroupfirstoccurences h
+                    LEFT JOIN main_requesterprofile p ON h.requester_id = p.requester_id
+            WHERE
+                h.occurrence_date > %s
+            GROUP BY
+                h.requester_id, h.requester_name, p.is_public
+            ORDER BY
+                sum(h.hits_available*reward)
+                DESC
+            LIMIT 1000;
+            ''', date_30_days_before))
 
     columns = (
         ('string','Requester ID'),
@@ -55,12 +59,12 @@ def top_requesters(request):
         ('datetime', 'Last Posted On'),
         ('string', 'Status'),
     )
-
-    return direct_to_template(request, 'main/graphs/table.html', {
-                                                                  'data':data,
-                                                                  'columns':columns,
-                                                                  'title':'Top-1000 Recent Requesters'
-                                                                  })
+    ctx = {
+        'data': data,
+        'columns': columns,
+        'title': 'Top-1000 Recent Requesters',
+    }
+    return direct_to_template(request, 'main/graphs/table.html', ctx)
 
 @login_required
 def toggle_requester_status(request, id):
@@ -75,19 +79,22 @@ def requester_details(request, requester_id):
     def row_formatter(input):
         for cc in input:
             row = []
-            row.append('<a href="%s">%s</a>' % (reverse('hit_group_details',kwargs={'hit_group_id':cc[5]}) ,cc[0]))
+            url = reverse('hit_group_details',kwargs={'hit_group_id':cc[5]})
+            row.append('<a href="%s">%s</a>' % (url, cc[0]))
             row.extend(cc[1:5])
             url = reverse('admin-toggle-hitgroup-status', args=(cc[5],))
             row.append('<a href="%s">%s</a>' % (url, cc[6] and 'public' or 'private'))
             yield row
 
-    requster_name = HitGroupContent.objects.filter(requester_id = requester_id).values_list('requester_name',flat=True).distinct()
+    requster_name = HitGroupContent.objects.filter(requester_id=requester_id)
+    requster_name = requster_name.values_list('requester_name',flat=True).distinct()
 
-    if requster_name: requster_name = requster_name[0]
-    else: requster_name = requester_id
+    if requster_name:
+        requster_name = requster_name[0]
+    else:
+        requster_name = requester_id
 
-    date_from = (datetime.date.today() - datetime.timedelta(days=30)).isoformat()
-
+    date_from = datetime.date.today() - datetime.timedelta(days=30)
     data = query_to_tuples("""
         SELECT
             title, hits_available, p.reward, p.occurrence_date,
@@ -96,10 +103,10 @@ def requester_details(request, requester_id):
             main_hitgroupfirstoccurences p
                 JOIN main_hitgroupcontent q ON (p.group_content_id = q.id AND p.requester_id = q.requester_id)
         WHERE
-            p.requester_id = '%s'
-            AND p.occurrence_date > TIMESTAMP '%s'
-            AND q.occurrence_date > TIMESTAMP '%s'
-        """ % (requester_id, date_from, date_from))
+            p.requester_id = %s
+            AND p.occurrence_date > %s
+            AND q.occurrence_date > %s
+        """, requester_id, date_from, date_from)
 
     columns = (
         ('string', 'HIT Title'),
@@ -113,10 +120,10 @@ def requester_details(request, requester_id):
     ctx = {
         'data': text_row_formater(row_formatter(data)),
         'columns': tuple(columns),
-        'title':'Last 100 Tasks posted by %s' % (requster_name),
+        'title': 'Last 100 Tasks posted by %s' % requster_name,
         'user': request.user,
     }
-    return direct_to_template(request, 'main/requester_details.html',ctx)
+    return direct_to_template(request, 'main/requester_details.html', ctx)
 
 @login_required
 def toggle_hitgroup_status(request, id):
