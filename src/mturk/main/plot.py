8f36eb1a48413fb97e7d877cf6865dd6dcd7b67c
Piotr Husiatyński <phusiatynski@gmail.com>
2010-12-01 15:50:44 +0100

    fix for iterators support in anomaly implementation

--- src/mturk/main/plot.py
+++ src/mturk/main/plot.py
@@ -12,11 +12,18 @@ def repair(data, is_anomaly):
     This will never return first and last two items from the given list, but
     you shouldn't care for large amount of data anyway.
     """
-    # zip 3 values from list sorder by indexes: [i, i+1, i+2]
-    iter = itertools.izip(
-            itertools.islice(data, 1, None), itertools.islice(data, 2, None))
 
-    a = data[0]
+    # support both lists and generators
+    try:
+        a = data[0]
+        # zip 3 values from list sorder by indexes: [i, i+1, i+2]
+        iter = itertools.izip(
+                itertools.islice(data, 1, None), itertools.islice(data, 2, None))
+    except TypeError:
+        a = data.next()
+        # zip 3 values from list sorder by indexes: [i, i+1, i+2]
+        iter = itertools.izip(data, itertools.islice(data, 1, None))
+
     for b, c in iter:
         if not is_anomaly(a, b, c):
             # if `b` is not anomaly, the assign it to `a`, because it's what
