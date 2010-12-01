edf38ed08140158f57fb6545c43e7e6215c491b6
Piotr Husiatyński <phusiatynski@gmail.com>
2010-12-01 11:05:26 +0100

    delete dummy aggregates

--- src/mturk/main/management/commands/__init__.py
+++ src/mturk/main/management/commands/__init__.py
@@ -155,31 +155,34 @@ def update_first_occured_agregates():
 
 
 def update_crawl_agregates(commit_threshold=10, only_new = True):
+    try:
 
-        try:
+        results = None
 
-            results = None
+        if only_new:
+            results = query_to_dicts("select id from main_crawl p where old_id is null and not exists(select id from main_crawlagregates where crawl_id = p.id)")
+        else:
+            results = query_to_dicts("select id from main_crawl p where not exists(select id from main_crawlagregates where crawl_id = p.id)")
 
-            if only_new:
-                results = query_to_dicts("select id from main_crawl p where old_id is null and not exists(select id from main_crawlagregates where crawl_id = p.id)")
-            else:
-                results = query_to_dicts("select id from main_crawl p where not exists(select id from main_crawlagregates where crawl_id = p.id)")
+        for i, row in enumerate(results):
 
-            for i, row in enumerate(results):
+            execute_sql("""insert into main_crawlagregates
+    select sum(hits_available) as "hits", start_time, sum(reward*hits_available) as "reward", crawl_id, nextval('main_crawlagregates_id_seq'), count(*) as "count"
+        from hits_mv p
+        where crawl_id = %s
+        group by crawl_id, start_time
+            """ % row['id'])
 
-                execute_sql("""insert into main_crawlagregates
-        select sum(hits_available) as "hits", start_time, sum(reward*hits_available) as "reward", crawl_id, nextval('main_crawlagregates_id_seq'), count(*) as "count"
-            from hits_mv p
-            where crawl_id = %s
-            group by crawl_id, start_time
-                """ % row['id'])
-
-                if i % commit_threshold == 0:
-                    logging.debug( 'commited after %s crawls' % i )
-                    execute_sql('commit;')
+            if i % commit_threshold == 0:
+                logging.debug( 'commited after %s crawls' % i )
+                execute_sql('commit;')
 
 
-        except:
-            error_info = grab_error(sys.exc_info())
-            logging.error('an error occured at crawl_id: %s, %s %s' % (row['id'],error_info['type'], error_info['value']))
-            execute_sql('rollback;')
+    except:
+        error_info = grab_error(sys.exc_info())
+        logging.error('an error occured at crawl_id: %s, %s %s' % (row['id'],error_info['type'], error_info['value']))
+        execute_sql('rollback;')
+
+    # delete dummy data
+    execute_sql("DELETE FROM main_crawlagregates WHERE projects < 200;")
+    execute_sql("COMMIT;")
