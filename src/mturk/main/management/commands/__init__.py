28172b62ff6253d20fd498770087da9439d3057a
Michał Kłujszo <michal.klujszo@10clouds.com>
2011-02-16 19:41:09 +0100

    fix

--- src/mturk/main/management/commands/__init__.py
+++ src/mturk/main/management/commands/__init__.py
@@ -156,42 +156,42 @@ def update_first_occured_agregates():
 
 def update_crawl_agregates(commit_threshold=10, only_new = True):
 
-        results = None
-
-        if only_new:
-            results = query_to_dicts("select id from main_crawl p where old_id is null and not exists(select id from main_crawlagregates where crawl_id = p.id)")
-        else:
-            results = query_to_dicts("select id from main_crawl p where not exists(select id from main_crawlagregates where crawl_id = p.id)")
-
-        logging.info("got %s results" % len(results))
-
-        for i, row in enumerate(results):
-            try:
-
-                execute_sql("""
-                INSERT INTO
-                    main_crawlagregates
-                SELECT
-                    sum(hits_available) as "hits",
-                    start_time,
-                    sum(reward * hits_available) as "reward",
-                    crawl_id,
-                    nextval('main_crawlagregates_id_seq'),
-                    count(*) as "count"
-                FROM
-                    (SELECT DISTINCT ON (group_id) * FROM hits_mv WHERE crawl_id = %s) AS p
-                GROUP BY
-                    crawl_id, start_time
-                """, row['id'])
+    results = None
     
-                if i % commit_threshold == 0:
-                    logging.debug( 'commited after %s crawls' % i )
-                    execute_sql('commit;')
-
-            except:
-                error_info = grab_error(sys.exc_info())
-                logging.error('an error occured at crawl_id: %s, %s %s' % (row['id'],error_info['type'], error_info['value']))
-                execute_sql('rollback;')
+    if only_new:
+        results = query_to_dicts("select id from main_crawl p where old_id is null and not exists(select id from main_crawlagregates where crawl_id = p.id)")
+    else:
+        results = query_to_dicts("select id from main_crawl p where not exists(select id from main_crawlagregates where crawl_id = p.id)")
+    
+    logging.info("got %s results" % len(results))
+    
+    for i, row in enumerate(results):
+        try:
+    
+            execute_sql("""
+            INSERT INTO
+                main_crawlagregates
+            SELECT
+                sum(hits_available) as "hits",
+                start_time,
+                sum(reward * hits_available) as "reward",
+                crawl_id,
+                nextval('main_crawlagregates_id_seq'),
+                count(*) as "count"
+            FROM
+                (SELECT DISTINCT ON (group_id) * FROM hits_mv WHERE crawl_id = %s) AS p
+            GROUP BY
+                crawl_id, start_time
+            """, row['id'])
+    
+            if i % commit_threshold == 0:
+                logging.debug( 'commited after %s crawls' % i )
+                execute_sql('commit;')
+    
+        except:
+            error_info = grab_error(sys.exc_info())
+            logging.error('an error occured at crawl_id: %s, %s %s' % (row['id'],error_info['type'], error_info['value']))
+            execute_sql('rollback;')
 
 
     # delete dummy data
