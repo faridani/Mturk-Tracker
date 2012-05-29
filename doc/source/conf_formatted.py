f1e0362b9438e6155e7fcb8d4424b4cfd734e732
Grzegorz Bagrowski <gbagrowski@gmail.com>
2012-05-29 16:05:18 +0200

    MTRACKER-12: Configuration update.
    Configuration file conf_formatted.py will now use the current settings
    file.

--- doc/source/conf_formatted.py
+++ doc/source/conf_formatted.py
@@ -12,7 +12,7 @@ DOC_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 # If this file is uploaded while deploying,
 # project_dir will be na absolute path
-if "%(project_dir)s".find('/') > 0:
+if "%(project_dir)s".find('/') != -1:
     ROOT_PATH = os.path.join("%(project_dir)s", 'code')
 else:
     ROOT_PATH = os.path.abspath(os.path.dirname(DOC_PATH))
@@ -21,6 +21,10 @@ sys.path.insert(0, os.path.join(ROOT_PATH, 'deployment'))
 sys.path.insert(0, os.path.join(ROOT_PATH, 'app'))
 sys.path.insert(0, os.path.join(ROOT_PATH))
 
-from mtracker.settings import development as settings
+settings_name = "%(settings_name)s"
+if settings_name.find('(settings_name)s') != -1:
+    settings_name = 'development'
+
+settings = __import__("mtracker.settings.{0}".format(settings_name))
 from django.core.management import setup_environ
 setup_environ(settings)
